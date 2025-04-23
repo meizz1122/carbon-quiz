68c245772a5d8a3e3b206ef2d6b9918d7109c460
Mei <meizi.zhang@tufts.edu>
2025-04-23 14:09:07 -0400

    Fixed gitignore

--- myapp/admin.py
+++ myapp/admin.py
@@ -8,5 +8,9 @@ from .models import Quiz_Question, Quiz_Choice, Quiz_User, Quiz_Response
 
 admin.site.register(Quiz_Question)
 admin.site.register(Quiz_Choice)
-admin.site.register(Quiz_User)
 admin.site.register(Quiz_Response)
+
+class UserAdmin(admin.ModelAdmin):
+    readonly_fields=('created_at',)
+
+admin.site.register(Quiz_User, UserAdmin)
