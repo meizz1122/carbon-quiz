f655294c35d5acfe23d843a0f636c2409d5a5f13
Mei <meizi.zhang@tufts.edu>
2025-07-17 12:34:19 -0400

    added models, fixed quiz background

--- myapp/scripts/choices_import.py
+++ myapp/scripts/choices_import.py
@@ -1,2 +1,10 @@
 # tbd to auto load question/choices from google sheets into model
 # create __init__.py file in directory as well to be able to later import functions writtens here as a python package
+
+import os
+from pathlib import Path
+
+def test1():
+    BASE_DIR = Path(__file__).resolve().parent.parent
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
+    print(STATIC_ROOT)
