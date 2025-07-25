7f658b64ede02d5caeb90542d273cb1d755d0939
Mei <meizi.zhang@tufts.edu>
2025-07-25 16:22:12 -0400

    fix urls

--- mysite/urls.py
+++ mysite/urls.py
@@ -26,6 +26,6 @@ from django.urls import path, include
 # The idea behind include() is to make it easy to plug-and-play URLs.
 
 urlpatterns = [
-    path("myapp/", include("myapp.urls")),
+    path('', include("myapp.urls")),
     path('admin/', admin.site.urls),
 ]
